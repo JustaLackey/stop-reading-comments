ce5eda93740a2f3790e21efe0e18334316d0472d
JustaLackey <tsavij@gmail.com>
2020-07-08 18:34:17 -0400

    init

---
+++ stopreadingcomments.js
@@ -0,0 +1,14 @@
+
+var comments = document.getElementsByClassName('comments');
+
+for (var i = 0; i < comments.length; i ++) {
+    comments[i].style.display = 'none';
+}
+
+
+var commentlink = document.getElementsByClassName('reddit-comment-link');
+
+for (var i = 0; i < commentlink.length; i ++) {
+    commentlink[i].style.display = 'none';
+}
+
